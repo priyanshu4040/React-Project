44e7f6d71bc44eb72c85c772e23fc3ee539f9d53
Priyanshu <priyanshuchakole@gmail.com>
2025-05-16 14:29:38 +0530

    First React Project

---
+++ Counter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(0)
+
+  const increment = () => {
+
+    if (count < 20) {
+      count = count + 1;
+      console.log("value of count", count)
+      setCount(count);
+    }
+    else {
+      console.log("Count value cannot be above 20")
+    }
+
+  }
+
+  const decrement = () => {
+    if (count > 0) {
+      count = count - 1
+      console.log("value of count", count)
+      setCount(count)
+    }
+    else {
+      console.log("Count value cannot be negative")
+    }
+  }
+
+  return (
+    <>
+      <h1>Count : {count}</h1>
+      <button onClick={increment}>Add Count</button>
+      <br />
+      <br />
+      <button onClick={decrement}>Reduce Count</button>
+    </>
+  )
+}
+
+export default App
